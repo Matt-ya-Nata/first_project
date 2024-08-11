8ea9283a09530e8d90b2fba489ad82ae57942db0
Mateu <mathewsmaps@gmail.com>
2024-08-11 02:46:28 -0800

    Basic Math Operators

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,27 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
+
+  print(9%4);
 }
+
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   //Variables : You can store different types of information in variables.
-    String name = "Mateus Natanael";
-    int age = 27;
-    double pi = 3.14159;
+  //   String name = "Mateus Natanael";
+  //   int age = 37;
+  //   double pi = 3.14159;
+
+  /*
+  Basic Math Operators
+  1 + 1 => 2, add
+  3 - 2 => 1, subtract
+  3 * 2 => 6, multiply
+  8 / 2 => 4, divide
+  9 % 4 => 1,remainder
+   */
 
   @override
   Widget build(BuildContext context) {
