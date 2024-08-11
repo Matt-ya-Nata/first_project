550b1d9b0e9ee409b5fd6140ac83ccf1ae773d31
Mateu <mathewsmaps@gmail.com>
2024-08-11 03:16:01 -0800

    Logical Operators

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 
   print(9==9);
 }
 
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+   MyApp({super.key});
 
   //Variables : You can store different types of information in variables.
-  //   String name = "Mateus Natanael";
-  //   int age = 37;
-  //   double pi = 3.14159;
+    String name = "Mateus Natanael";
+    int age = 37;
+    double pi = 3.14159;
+    bool isBeginner = true;
 
   /*
   Basic Math Operators
@@ -31,12 +32,23 @@ class MyApp extends StatelessWidget {
   5 >= 5 -> true, GREATER THAN OR EQUAL TO
   3 <= 7 -> true, LESS THAN OR EQUAL TO
 
+  Logical Operators
+  AND operator , returns tru if both sides are true
+  isBeginner && (age < 20) -> return false
+
+  OR operator , returns true if at least one side is true
+  isBeginner || (age <18) -> return true
+
+  Not operator ,returns the opposite value
+  !isBeginer -> return False
+
    */
 
 
 
   @override
   Widget build(BuildContext context) {
+    print(!isBeginner || (age<18));
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(),
